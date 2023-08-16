391cf4c7016f62853ec6d8850f455e31f3cae133
Maja Kabus <mkabus@cern.ch>
2023-08-16 09:31:22 +0200

    Fixed log sigma plots with stats

--- src/pdi/plot_nsigma.py
+++ src/pdi/plot_nsigma.py
@@ -1,15 +1,8 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import mpl_scatter_density # adds projection='scatter_density'
-from matplotlib.colors import LinearSegmentedColormap
-
 import datashader as ds
 from datashader.mpl_ext import dsshow
-
-import matplotlib as mpl
-mpl.rcParams['agg.path.chunksize'] = 10000
 
 INPUT_PATH = "../PID_in_O2/LHC18g4_train246_mc_multiple_detectors.csv"
 PART_DICT = {
@@ -25,8 +18,8 @@ PART_DICT = {
     -2212: "Pr",
 }
 NSIGMA_COLUMNS = [
-     *["fTPCNSigma" + val for val in PART_DICT.values()],
-     *["fTOFNSigma" + val for val in PART_DICT.values()]]
+    *["fTPCNSigma" + val for val in PART_DICT.values()],
+    *["fTOFNSigma" + val for val in PART_DICT.values()]]
 
 CSV_DELIMITER = ","
 
@@ -36,23 +29,31 @@ for part in PART_DICT:
     for det in ["fTPCNSigma", "fTOFNSigma"]:
         nsigma = det + PART_DICT[part]
         df_part = df.loc[df["fPdgCode"] == part]
+        count = df_part[nsigma].size
+        mean_y = round(df_part[nsigma].mean(), 5)
+        std_dev_y = round(df_part[nsigma].std(), 5)
+        mean_x = round(df_part["fPt"].mean(), 5)
+        std_dev_x = round(df_part["fPt"].std(), 5)
 
         fig, ax = plt.subplots()
         dsartist = dsshow(
-                df_part,
-                ds.Point("fPt", nsigma),
-                ds.count(),
-                norm="log",
-                aspect="auto",
-                ax=ax,
+            df_part,
+            ds.Point("fPt", nsigma),
+            ds.count(),
+            norm="log",
+            aspect="auto",
+            ax=ax,
             )
-        plt.colorbar(dsartist)
+        plt.colorbar(dsartist, label="entries")
+        plt.figtext(0.5, 0.75, f"Entries    {count}\nMean x     {mean_x}\nStd Dev x   {std_dev_x}"\
+                    f"Mean y    {mean_y}\nStd Dev y   {std_dev_y}",
+                    bbox={"facecolor":"white", "pad":5})
 
-        plt.xlim([0.0, 10])
+        plt.xlim([0.0, 5.0])
         plt.ylim([-3.5, 3.5])
         plt.xlabel("pT")
         plt.ylabel(nsigma)
         plt.title(f"{nsigma} vs pT")
 
-        plt.savefig(f"nsigma_{nsigma}_pt.png")
+        plt.savefig(f"nsigma_{nsigma}_pt.png", bbox_inches="tight")
         plt.close()
